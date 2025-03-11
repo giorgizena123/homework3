8751c2289c47fdb87e7bdc56030a95d0f0864314
giorgizena123 <zenaishvili621@gmail.com>
2025-03-11 23:08:22 +0400

    Update Student.java

--- src/Student.java
+++ src/Student.java
@@ -1,4 +1,27 @@
-package PACKAGE_NAME;
+import java.util.*;
+class Student implements Comparable<Student> {
+    String firstName, lastName;
+    Date birthDate;
+    ArrayList<Integer> grades;
 
-public class Student {
+    public Student(String firstName, String lastName, Date birthDate, ArrayList<Integer> grades) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthDate = birthDate;
+        this.grades = grades;
+    }
+
+    public double getAverageGrade() {
+        return grades.stream().mapToInt(Integer::intValue).average().orElse(0);
+    }
+
+    @Override
+    public int compareTo(Student o) {
+        return (this.firstName + this.lastName).compareTo(o.firstName + o.lastName);
+    }
+
+    @Override
+    public String toString() {
+        return firstName + " " + lastName + " - Avg Grade: " + getAverageGrade();
+    }
 }
