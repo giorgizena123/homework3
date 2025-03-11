e45814c4a2e967aba25e4462710129a8f0626835
giorgizena123 <zenaishvili621@gmail.com>
2025-03-11 23:07:37 +0400

    Update Car.java

--- src/Car.java
+++ src/Car.java
@@ -1,4 +1,23 @@
-package PACKAGE_NAME;
+class Car implements Comparable<Car> {
+    String brand, model;
+    int year, mileage;
+    Engine engine;
 
-public class Car {
+    public Car(String brand, String model, int year, int mileage, Engine engine) {
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+        this.mileage = mileage;
+        this.engine = engine;
+    }
+
+    @Override
+    public int compareTo(Car o) {
+        return Integer.compare(this.year, o.year);
+    }
+
+    @Override
+    public String toString() {
+        return brand + " " + model + " - Year: " + year + ", Mileage: " + mileage + ", Engine: " + engine.capacity + "L";
+    }
 }
