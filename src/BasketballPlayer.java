2ebe1097026cb8b6d29b212fda5e14319b4e2709
giorgizena123 <zenaishvili621@gmail.com>
2025-03-11 23:07:25 +0400

    Update BasketballPlayer.java

--- src/BasketballPlayer.java
+++ src/BasketballPlayer.java
@@ -1,4 +1,29 @@
-package PACKAGE_NAME;
+import java.util.*;
 
-public class BasketballPlayer {
+class BasketballPlayer implements Comparable<BasketballPlayer> {
+    String firstName, lastName;
+    int points, rebounds, blocks, assists, turnovers;
+
+    public BasketballPlayer(String firstName, String lastName, int points, int rebounds, int blocks, int assists, int turnovers) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.points = points;
+        this.rebounds = rebounds;
+        this.blocks = blocks;
+        this.assists = assists;
+        this.turnovers = turnovers;
+    }
+    public double getRating() {
+        return points + rebounds + (assists * 1.5) + (blocks * 2) - (turnovers * 2);
+    }
+
+    @Override
+    public int compareTo(BasketballPlayer o) {
+        return Double.compare(o.getRating(), this.getRating());
+    }
+
+    @Override
+    public String toString() {
+        return firstName + " " + lastName + " - Rating: " + getRating();
+    }
 }
